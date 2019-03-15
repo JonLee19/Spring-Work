d9d8dfa7f3b61c2ec481848d03e1f7ffb718d863
Poxo <Poxo@DESKTOP-M7CKPDT>
2019-03-14 19:44:19 -0700

    Removed extra system.out.print test lines 3/14/19

--- TextExcel/src/textExcel/TextExcel.java
+++ TextExcel/src/textExcel/TextExcel.java
@@ -19,22 +19,7 @@ public class TextExcel
     	//open scanner to take in an expression
     	while (!input.equalsIgnoreCase("quit")) {
     		//repeat the loop until the user enters quit
-    		/*input = "A1 = -9";
     		System.out.println(sheet.processCommand(input));
-    		input = "A2 = ( 14 - 7 + -4 - 3 + 3 * A1 )";
-    		System.out.println(sheet.processCommand(input));
-    		input = "B1 = ( avG A2-a2 )";
-    		System.out.println(sheet.processCommand(input));
-    		*/
-    		 sheet.processCommand("F8 = 3");
-             sheet.processCommand("G8 = ( 5 )");
-             System.out.println(sheet.getGridText());
-             sheet.processCommand("H8 = ( 2 * -3 + 4 - -2 + -1 * F8 + G8 )"); // 2
-             System.out.println(sheet.getGridText());
-             sheet.processCommand("I8 = ( sum F8-H8 )"); // 10
-             System.out.println(sheet.getGridText());
-             sheet.processCommand("J8 = ( AVG F8-I8 )"); // 5
-             System.out.println(sheet.getGridText());
     		System.out.println("Type \"quit\" to end or new values to try again.");
     		input = console.nextLine().trim();
     	}                                          
